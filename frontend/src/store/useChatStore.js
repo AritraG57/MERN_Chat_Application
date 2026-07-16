4a97cd0b846784a5a8cfee12e9a0588519867e2b
AritraG57 <aritraghosh2005@gmail.com>
2026-07-16 11:42:35 +0530

    Implemented Socket.io

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import toast from "react-hot-toast";
 import { axiosInstance } from "../lib/axios";
+import { useAuthStore } from "./useAuthStore";
 
 export const useChatStore = create((set,get) => ({
     messages: [],
@@ -31,7 +32,6 @@ export const useChatStore = create((set,get) => ({
             set({isMessagesLoading: false});
         }
     },
-    //optimize later
     setSelectedUser: (selectedUser) => set({selectedUser}),
     sendMessage: async (messageData) => {
         const {selectedUser,messages} = get();
@@ -42,4 +42,24 @@ export const useChatStore = create((set,get) => ({
             toast.error(error.response.data.message);
         }
     },
+    subscribeToMessages: () => {
+        const { selectedUser } = get();
+        if(!selectedUser) return;
+
+        const socket = useAuthStore.getState().socket;
+
+        socket.on("newMessage", (newMessage) => {
+            const isMessageSentFromSelectedUser = newMessage.senderId === selectedUser._id;
+
+            if(!isMessageSentFromSelectedUser) return;
+
+            set({
+                messages: [...get().messages,newMessage],
+            });
+        });
+    },
+    unsubscribeFromMessages: () => {
+        const socket = useAuthStore.getState().socket;
+        socket.off("newMessage");
+    }
 }));
